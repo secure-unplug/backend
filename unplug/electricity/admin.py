f49824c7815f8d61b30e21cf6b7b95ffc8678038
SeongYong <yyyy7246@naver.com>
2022-08-19 04:11:54 +0900

    Metadata 테이블 추가

--- unplug/electricity/admin.py
+++ unplug/electricity/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Entries
+from .models import Metadata
 
 admin.site.register(Entries)
-
+admin.site.register(Metadata)
--- unplug/electricity/models.py
+++ unplug/electricity/models.py
@@ -1,11 +1,27 @@
 from django.db import models
 from datetime import datetime
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 class Entries(models.Model):
     uuid = models.CharField(max_length=200)
     watt = models.IntegerField()
     created_at = models.DateTimeField(default=datetime.now())
+
     class Meta:
-        db_table="index"
+        db_table = "index"
+
     def __str__(self):
         return f'[{self.uuid}]{self.created_at}'
+
+
+class Metadata(models.Model):
+    month = models.IntegerField(validators=[MinValueValidator(1),
+                                            MaxValueValidator(12)])
+    average_Kwatt = models.IntegerField()
+
+    class Meta:
+        db_table = "metadata"
+
+    def __str__(self):
+        return f'{self.month}월 사용량'
