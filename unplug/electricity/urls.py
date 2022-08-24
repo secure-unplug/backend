581155f3f37bc325ac4217d3f45a4cf565873b1a
kyu <kbo002004@naver.com>
2022-08-25 07:04:54 +0900

    f

--- unplug/electricity/urls.py
+++ unplug/electricity/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from .views import save_entries, view_entries, view_average_money, view_device_data, view_average_kwatt, get_kwatt_level, view_period_average, view_device_fee
-
+from .views import save_entries, view_entries, view_average_money, view_device_data, view_average_kwatt, get_kwatt_level, view_period_average, view_device_fee, view_my_entries, view_my_entries
 urlpatterns = [
     path('list', view_entries),
     path('save', save_entries),
     path('average_money', view_average_money),
     path('device_data', view_device_data),
-    #path('my_entries', view_my_entries),
+    path('my_entries', view_my_entries),
     path('average_kwatt', view_average_kwatt),
     path('kwatt_level', get_kwatt_level),
     path('period_average',view_period_average),
